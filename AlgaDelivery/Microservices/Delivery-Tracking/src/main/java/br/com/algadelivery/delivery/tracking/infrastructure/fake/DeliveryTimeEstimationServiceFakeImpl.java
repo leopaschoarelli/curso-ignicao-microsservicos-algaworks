5bc70d69d105a61b23703927e1624927aceed0db
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-03-01 11:34:31 -0300

    Implementando Domain Service para integração

---
+++ AlgaDelivery/Microservices/Delivery-Tracking/src/main/java/br/com/algadelivery/delivery/tracking/infrastructure/fake/DeliveryTimeEstimationServiceFakeImpl.java
@@ -0,0 +1,18 @@
+package br.com.algadelivery.delivery.tracking.infrastructure.fake;
+
+import br.com.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import br.com.algadelivery.delivery.tracking.domain.service.DeliveryEstimate;
+import br.com.algadelivery.delivery.tracking.domain.service.DeliveryTimeEstimationService;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class DeliveryTimeEstimationServiceFakeImpl implements DeliveryTimeEstimationService {
+
+    @Override
+    public DeliveryEstimate estimate(ContactPoint sender, ContactPoint receiver) {
+        return new DeliveryEstimate(Duration.ofHours(3), 3.1);
+    }
+
+}
