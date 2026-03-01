0078aa54696fcea362bacdd190a43897a62c113f
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-03-01 12:05:58 -0300

    Comunicação síncrona com Spring RestClient

---
+++ AlgaDelivery/Microservices/Delivery-Tracking/src/main/java/br/com/algadelivery/delivery/tracking/infrastructure/http/client/CourierPayoutResultModel.java
@@ -0,0 +1,16 @@
+package br.com.algadelivery.delivery.tracking.infrastructure.http.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CourierPayoutResultModel {
+
+    private BigDecimal payoutFee;
+
+}
