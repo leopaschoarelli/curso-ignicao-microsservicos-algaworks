755760fb715bcbcde73852b05f963d2bb14b6680
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-03-01 09:23:38 -0300

    Implementando a camada de domínio para Courier

---
+++ AlgaDelivery/Microservices/Courier-Management/src/main/java/br/com/algadelivery/courier/management/domain/model/AssignedDelivery.java
@@ -0,0 +1,27 @@
+package br.com.algadelivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AssignedDelivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    static AssignedDelivery pending(UUID deliveryId) {
+        var delivery = new AssignedDelivery();
+
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+
+        return delivery;
+    }
+}
