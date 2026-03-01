3d2b1b525afbc2ffe72bd2ba463a173f70f8cc97
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-03-01 15:05:55 -0300

    Implementando producer

--- AlgaDelivery/Microservices/Delivery-Tracking/src/main/java/br/com/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
+++ AlgaDelivery/Microservices/Delivery-Tracking/src/main/java/br/com/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
@@ -8,24 +8,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import static br.com.algadelivery.delivery.tracking.infrastructure.kafka.KafkaTopicConfig.DELIVERY_EVENTS_TOPIC;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class DeliveryDomainEventHandler {
 
+    private final IntegrationEventPublisher integrationEventPublisher;
+
     @EventListener
     public void handle(DeliveryPlacedEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), DELIVERY_EVENTS_TOPIC);
     }
 
     @EventListener
     public void handle(DeliveryPickUpEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), DELIVERY_EVENTS_TOPIC);
     }
 
     @EventListener
     public void handle(DeliveryFulfilledEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), DELIVERY_EVENTS_TOPIC);
     }
 
 }
