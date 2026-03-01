8a408c8bbea554b197a2b95cb14e0133f897b7b6
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-03-01 10:33:13 -0300

    Implementando endpoint para cadastro de uma Delivery

---
+++ AlgaDelivery/Microservices/Delivery-Tracking/src/main/java/br/com/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -0,0 +1,25 @@
+package br.com.algadelivery.delivery.tracking.api.controller;
+
+import br.com.algadelivery.delivery.tracking.domain.model.Delivery;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/deliveries")
+public class DeliveryController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Delivery draft(@RequestBody @Valid DeliveryInput deliveryInput) {
+        return null;
+    }
+
+    @PutMapping("/{deliveryId}")
+    public Delivery edit(@PathVariable UUID deliveryId,
+                         @RequestBody @Valid DeliveryInput deliveryInput) {
+    }
+
+}
